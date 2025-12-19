533d47381a8b35bea4b7f58858e493fa67c30c3a
Ezekiel Kirui <shadikirui0@gmail.com>
2025-12-19 10:06:21 +0300

    004/19 allowed host changes

--- Front-end/src/services/url.ts
+++ Front-end/src/services/url.ts
@@ -1,3 +1,4 @@
 let LOCAL_API = "http://127.0.0.1:8080/api"
-let PRODUCTION_API = "https://laundry12-40.onrender.com/api"
-export const API_BASE_URL = LOCAL_API || PRODUCTION_API ;
+let PRODUCTION_API1 = "https://clean-page-laundry:10000/api" 
+let PRODUCTION_API2 = "https://cleanpage.shop/api"
+export const API_BASE_URL = LOCAL_API || PRODUCTION_API1 || PRODUCTION_API2;
